9fa8097e2e8be12fe6d1d75da51a8ea47ba6be29
codewithsaffy <codewithsaffy@gmail.com>
2024-11-19 16:19:44 +0500

    add

--- laiba project.py2/main.py
+++ laiba project.py2/main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import naatlibrary
 #pip install pocketsphinx
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -8,23 +9,48 @@ engine = pyttsx3.init()
 def speak(text):
   engine.say(text)
   engine.runAndWait()
+
+def process_command(c): 
+  if "open google" in c.lower():
+    webbrowser.open("https://google.com")
+  elif "open youtube" in c.lower():
+    webbrowser.open("https://youtube.com")
+  elif "open tiktok" in c.lower():
+    webbrowser.open("https://tiktok.com")
+  elif "open facebook" in c.lower():
+    webbrowser.open("https://facebook.com")
+  elif "open linkedin" in c.lower():
+    webbrowser.open("https://linkedin.com")
+  elif c.lower().startswith("play"):
+    naat = c.lower().split(" ")[1]
+    link=naatlibrary=naat[song]
+    webbrowser.open(link)
+
+  
 if __name__ == "__main__":
   speak("Initialing jarvis....")
-  #listen for the wake word"jarvis
-  #obtain audio from the microphone
+    #listen for the wake word"jarvis
+    #obtain audio from the microphone
   r=sr.Recognizer()
-  with sr.Microphone() as source:
-    print("listening...")
-    audio = r.listen(source)
-    
-  # recognize speech using Sphinx
-  try:
-    command = r.recognize_google(audio)
-    print(command)
-  except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-  except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))  
+
+  while True:
+    print("recognizing...")
+    try:
+      with sr.Microphone() as source:
+        print("listening...")
+        audio = r.listen(source,timeout=5,phrase_time_limit=5)
+      word = r.recognize_google(audio)
+      if (word.lower() == "jarvis"):
+        speak("yes")
+      #listen for command
+        with sr.Microphone() as source:
+          print("jarvis active...")
+          audio = r.listen(source , timeout=2, phrase_time_limit=1)
+          command = r.recognize_google(audio)
+
+          process_command(command)
+    except Exception as e:
+      print(" error; {0}".format(e))  
 
 
 
---
+++ laiba project.py2/naatlibrary.py
@@ -0,0 +1,7 @@
+naat={
+  "AssubhuBada":"https://www.youtube.com/watch?v=T1bnRJFNjic",
+  "kunfayakun":"https://www.youtube.com/watch?v=29nov3l4PDk",
+  "muhammadnabina":"https://www.youtube.com/watch?v=xvtmMA1zb-k"
+
+
+}
